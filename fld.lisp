b8aaaab3b6d7573e3c46b71ac36b78bc32c8b918
Rigidus <avenger-f@yandex.ru>
2011-05-25 16:49:10 +0400

    fld.lisp

---
+++ fld.lisp
@@ -0,0 +1,49 @@
+(defpackage #:fld
+  (:use #:cl)
+  (:export :field
+           :view
+           :update
+           :controller
+           ))
+
+(in-package #:fld)
+
+
+;; Допустимые типы полей, составляющих сущности (TODO: при создании экземпляра entity написать
+;; проверку, чтобы тип входил в этот список)
+(defparameter *types*
+  '(:str ;; строка
+    :num ;; число
+    :lst ;; список
+    ))
+
+(defparameter *validators* nil) ;; Валидаторы, определенные для полей
+
+(defclass field ()
+  ((name         :initarg :name        :initform "unknown"           :accessor name)
+   (value        :initarg :value       :initform ""                  :accessor value)
+   (caption      :initarg :caption     :initform "Неизвестное поле"  :accessor caption)
+   (typeclass    :initarg :typeclass   :initform :str                :accessor typeclass)
+   (validators   :initarg :validators  :initform nil                 :accessor validators)
+   ))
+
+;; TODO: name нужно проверять на только английские маленькие буквы (без пробелов)
+;; TODO: typeclass должен попадать в список разрешенных типов, валидаторы должны быть заранее определены
+
+;; Возвращает html-код для просмотра поля
+(defmethod view ((fld field))
+  (format nil "<tr><td>~A</td><td>~A</td></tr>"
+          (caption fld)
+          (value fld)))
+
+;; Возвращает html-код для редактирования поля
+(defmethod update ((fld field))
+  (format nil "<tr><td><input type=\"text\" name=\"~A\" value=\"~a\" /></td></tr>"
+          (name fld)
+          (caption fld)))
+
+;; Возвращает функцию, которая применяет валидаторы к входному значению, и в случае успеха
+;; возвращает T, иначе NIL
+(defmethod controller ((fld field))
+  (lambda (value)
+    t))
