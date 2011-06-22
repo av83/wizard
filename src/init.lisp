53bd0f0882a13251d3a7ef03b2418f1f80bb0858
Rigidus <avenger-f@yandex.ru>
2011-06-22 15:39:37 +0400

    init

---
+++ src/init.lisp
@@ -0,0 +1,46 @@
+(in-package #:wizard)
+
+(setf (gethash (hash-table-count *USER*) *USER*)
+      (make-instance 'ADMIN
+                     :login "admin"
+                     :password "admin"))
+
+(setf (gethash (hash-table-count *USER*) *USER*)
+      (make-instance 'EXPERT
+                     :name "Эксперт-1"
+                     :login "exp1"
+                     :password "exp1"))
+
+(setf (gethash (hash-table-count *USER*) *USER*)
+      (make-instance 'EXPERT
+                     :name "Эксперт-2"
+                     :login "exp2"
+                     :password "exp2"))
+
+(setf (gethash (hash-table-count *USER*) *USER*)
+      (make-instance 'SUPPLIER
+                     :status :fair
+                     :name "Поставщик-1"
+                     :login "supp1"
+                     :password "supp1"))
+
+(setf (gethash (hash-table-count *USER*) *USER*)
+      (make-instance 'SUPPLIER
+                     :status :unfair
+                     :name "Поставщик-2"
+                     :login "supp2"
+                     :password "supp2"))
+
+(setf (gethash (hash-table-count *USER*) *USER*)
+      (make-instance 'SUPPLIER
+                     :status :request
+                     :name "Поставщик-3"
+                     :login "supp3"
+                     :password "supp3"))
+
+
+;; dbg out
+(maphash #'(lambda (k v)
+             (print (list k v (a-login v))))
+         *USER*)
+
